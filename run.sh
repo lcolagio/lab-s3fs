184afaa5d886b0c292f553a09bb237727f56723e
lcolagio <66462164+lcolagio@users.noreply.github.com>
2021-04-10 11:50:02 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 sleep 3600
-
+# mkdir -p "$MNT_POINT"
 # echo "$AWS_KEY:$AWS_SECRET_KEY" > tmp/passwd && chmod 600 tmp/passwd
 # s3fs "$S3_BUCKET" "$MNT_POINT" -o passwd_file=tmp/passwd  && tail -f /dev/null
